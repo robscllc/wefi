5bbb986bbe606019bcfed876a116bafb9f680039
Fred Boutros <fred@weekdayfiller.com>
2013-01-20 14:16:07 -0500

    cleaned up server/, added onCreateUser

---
+++ server/users.js
@@ -0,0 +1,14 @@
+Accounts.onCreateUser(function(options, user){
+  user.profile = options.profile || {};
+
+  if (options.email)
+    user.profile.email = options.email;
+    
+  if (!user.profile.name)
+    user.profile.name = user.username;
+  
+  if ( !Meteor.users.find().count() )
+    user.isAdmin = true;
+
+  return user;
+});
