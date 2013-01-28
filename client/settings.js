6cd8af295d3ea875c9b07d460af6edaff3b6f65f
Fred Boutros <fred@weekdayfiller.com>
2013-01-28 18:10:45 +0000

    added github and google logins

--- client/settings.js
+++ client/settings.js
@@ -1,10 +1,10 @@
 Accounts.ui.config({
-  // requestPermissions: {
-  //   facebook: ['user_likes'],
-  //   github: ['user', 'repo']
-  // },
-  // requestOfflineToken: {
-  //   google: true
-  // },
+  requestPermissions: {
+    facebook: ['user_likes'],
+    github: ['user', 'repo']
+  },
+  requestOfflineToken: {
+    google: true
+  },
   passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
 });
