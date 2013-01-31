fd0947357deafd00f5188fd89a6b728fd8e3e18e
Fred Boutros <fred@weekdayfiller.com>
2013-01-31 11:15:14 -0500

    removed img tag

--- client/thirdparty/pagedown/Markdown.Sanitizer.js
+++ client/thirdparty/pagedown/Markdown.Sanitizer.js
@@ -28,7 +28,7 @@
     var img_white = /^(<img\ssrc="(https?:\/\/|\/)[-A-Za-z0-9+&@#\/%?=~_|!:,.;\(\)]+"(\swidth="\d{1,3}")?(\sheight="\d{1,3}")?(\salt="[^"<>]*")?(\stitle="[^"<>]*")?\s?\/?>)$/i;
 
     function sanitizeTag(tag) {
-        if (tag.match(basic_tag_whitelist) || tag.match(a_white) || tag.match(img_white))
+        if (tag.match(basic_tag_whitelist) || tag.match(a_white))
             return tag;
         else
             return "";
