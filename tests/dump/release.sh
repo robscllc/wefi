ebea1a565cf894573a43fac52d9e0299001233b1
Fred Boutros <fred@weekdayfiller.com>
2013-02-05 21:45:00 -0500

    release script has multiple targets now

--- tests/dump/release.sh
+++ tests/dump/release.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 VERSION=$1
+TARGET=$2
+
+[ -z $VERSION ] && exit 1
+[ -z $TARGET ] && exit 1
+
 BDIR=~/bundles
 DDIR=~/deploy
 CUR=$(git branch | grep \* | awk '{ print $2 }')
@@ -19,8 +24,8 @@ find bundle -type d -print0 | xargs -0 chmod o+rx
 find bundle -type f -print0 | xargs -0 chmod o+r
 rm -rf $TITLE/
 mv bundle $TITLE || exit 1
-rm -f latest
-ln -s $TITLE latest
+rm -f $TARGET
+ln -s $TITLE $TARGET
 popd
 
 git checkout $CUR
